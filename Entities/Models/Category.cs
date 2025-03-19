d2e58ee4f71610d738ae649c477728c2905d4286
. Олег <oleg28082003@gmail.com>
2025-03-19 18:38:55 +0300

    update

---
+++ Entities/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; }
+        
+    }
+}
